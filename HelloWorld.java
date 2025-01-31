831a8dcf0bdcd9f1f343bd8cc7560447a2bac808
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-30 23:39:35 -0300

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld { // o nome da classe precisa ser compatível com o nome do arquivo
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
