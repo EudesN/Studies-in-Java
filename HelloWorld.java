babaffb6479508c2461aabf1ef9f9fd7f7762c17
Eudes Nazário <eudesnazario178@gmail.com>
2025-01-31 17:20:30 +0000

    Jan 31, 2025, 2:20 PM

--- HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld { // o nome da classe precisa ser compatível com o nome do arquivo
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
