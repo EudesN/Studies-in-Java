798a826a7f046ccb56046f67f7d7a49f2ca40ace
EudesN <eudesnazario178@gmail.com>
2025-02-19 18:16:05 +0000

    Feb 19, 2025, 3:16 PM

--- basic/src/IntroStrings.java
+++ basic/src/IntroStrings.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class IntroStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual é o seu primeiro nome?");
+        String primeiroNome = scanner.next();
+        System.out.println("Qual o seu último nome?");
+        String ultimoNome = scanner.next();
+
+
+
+        System.out.println(primeiroNome + " " + ultimoNome);
+    }
     
 }
