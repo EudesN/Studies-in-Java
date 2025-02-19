39f53ed692e2e9305823476515ba7a993016c6f8
EudesN <eudesnazario178@gmail.com>
2025-02-19 19:15:50 +0000

    Feb 19, 2025, 4:15 PM

--- Problemas/src/AnoBissexto.java
+++ Problemas/src/AnoBissexto.java
@@ -3,5 +3,33 @@ import java.util.Scanner;
 public class AnoBissexto{
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        int ano = scanner.nextInt();
+
+        boolean anoBissexto = false;
+
+        
+        if(ano % 400 == 0){
+            if(ano % 55 == 0){
+                System.out.println("This is bulukulu festival year.");
+                return;
+            }
+            System.out.println("This is leap year.");
+            return;
+        }
+        else{
+            if(ano % 4 == 0 && ano % 100 != 0){
+                if(ano % 55 == 0){
+                    System.out.println("This is bulukulu festival year.");
+                    return;
+                }
+                System.out.println("This is leap year.");
+                return;
+            }
+        }
+        if(ano % 15 == 0){
+            System.out.println("This is huluculu festival year.");
+            return;
+        }
+        System.out.println("This is a ordinary year.");
     }
 }
