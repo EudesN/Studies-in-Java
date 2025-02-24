23d35108c2ad800f7456c84a530a4dc83c6d5e68
EudesN <eudesnazario178@gmail.com>
2025-02-24 13:10:54 +0000

    Feb 24, 2025, 10:10 AM

--- basic/src/Matriz.java
+++ basic/src/Matriz.java
@@ -6,15 +6,15 @@ public class Matriz {
 
         double[][] vetor = new double[3][3];
 
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+        for(int i = 0; i < vetor.length; i++){
+            for(int j = 0; j < vetor[i].length; j++){
                 vetor[i][j] = input.nextDouble();
                 
             }
         }
 
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+        for(int i = 0; i < vetor.length; i++){
+            for(int j = 0; j < vetor[i].length; j++){
                 System.out.print(vetor[i][j]);
                 System.out.print(" ");
             }
